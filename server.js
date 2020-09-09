f3bc41b38323f8e40b3634db9e77cfb1736bfebe
Aakash Singh <aakashsingh717@gmail.com>
2020-09-09 21:21:16 +0530

    server is up

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express")
+const bodyParser = require("body-parser")
+const app = express()
+app.use(bodyParser.urlencoded({extended:true}))
+
+
+
+app.listen(3000)
+
+app.get("/",function(req,res){
+  res.send("Hello world")
+})
